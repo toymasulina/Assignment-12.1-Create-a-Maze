5032fd131e8bcfe0bd32b28557d48a4c5480e3c7
Robbie Harrison <toymasulina@gmail.com>
2023-05-10 12:20:12 -0400

    Mostly complete office setup. Still missing working doors

---
+++ Assets/Scripts/CheckWin.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckWin : MonoBehaviour
+{
+    public GameObject winMessage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        winMessage.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("MainCamera"))
+        {
+            if (!winMessage.activeInHierarchy)
+            {
+                winMessage.SetActive(true);
+            }
+        }
+    }
+}
